206b14d32014d86e3985f659377b5e6f3188c0b2
Mohammed hussein <mohammedhussein1225m@gmail.com>
2025-03-11 22:24:07 +0200

    edite

--- main.js
+++ main.js
@@ -1,178 +1,72 @@
-// // let li = document.querySelectorAll("ul li");
-// // let btn = document.querySelector("button");
+// create table and add data to page
 
-// // // send data to api
-// // btn.addEventListener("click", () => {
-// //   li.forEach((li) => {
-// //     fetch("https://67ce9cbb125cd5af757b3855.mockapi.io/product", {
-// //       method: "POST",
-// //       headers: {
-// //         "Content-type": "application/json",
-// //       },
+function table(data) {
+  let thead = document.querySelector("table thead");
+  thead.innerHTML = "";
+  let headers = ["ID", "First Name", "Last Name", "Email", " "];
 
-// //       body: JSON.stringify({
-// //         name: li.textContent,
-// //         age: 15,
-// //       }),
-// //     })
-// //       .then((response) => response.json())
-// //       .then((data) => console.log("post success", data))
-// //       .catch((error) => console.error("حدث خطأ", error));
-// //   });
-// // });
-
-// // // get data from api
-
-// // let get = document.querySelector(".get");
-// // get.addEventListener("click", () => {
-// //   fetch("https://67ce9cbb125cd5af757b3855.mockapi.io/product")
-// //     .then((response) => response.json())
-// //     .then((data) => {
-// //       show(data);
-// //     })
-// //     .catch((error) => console.error(error));
-// // });
-
-// // function show(products) {
-// //   products.sort();
-// //   products.forEach((pro) => {
-// //     let div = document.createElement("div"),
-// //       p = document.createElement("p");
-// //     p.innerHTML = pro.name;
-// //     div.appendChild(p);
-// //     document.body.appendChild(div);
-// //   });
-// // }
-
-// // // edit data from api
-// // let edit = document.querySelector(".edit");
-// // edit.addEventListener("click", () => {
-// //   fetch("https://67ce9cbb125cd5af757b3855.mockapi.io/product")
-// //     .then((response) => response.json())
-// //     .then((data) => {
-// //       data.forEach((pro) => {
-// //         fetch(
-// //           `https://67ce9cbb125cd5af757b3855.mockapi.io/product/${pro.age}`,
-// //           {
-// //             method: "PUT",
-// //             headers: {
-// //               "Content-type": "application/json",
-// //             },
-// //             body: JSON.stringify({
-// //               name: "ًWelcome to my website",
-// //             }),
-// //           }
-// //         );
-// //       });
-// //     })
-// //     .catch((error) => console.error(error));
-// // });
-
-// // // delete data from api
-// // let del = document.querySelector(".del");
-
-// // del.addEventListener("click", () => {
-// //   fetch("https://67ce9cbb125cd5af757b3855.mockapi.io/product/9")
-// //     .then((res) => res.json())
-// //     .then((data) => {
-// //       if (data) {
-// //         fetch("https://67ce9cbb125cd5af757b3855.mockapi.io/product/9", {
-// //           method: "PATCH",
-// //           headers: {
-// //             "Content-type": "application/json",
-// //           },
-// //           body: JSON.stringify({ age: null }),
-// //         })
-// //           .then((response) => response.json())
-// //           .catch((error) => console.log(" not del", error));
-// //       } else {
-// //         console.log("data not found");
-// //       }
-// //     })
-// //     .catch((error) => console.log("problem", error));
-// // });
-
-// function getdata() {
-//   return {
-//     users: [],
-
-//     inite() {
-//       fetch("https://reqres.in/api/users?page=1")
-//         .then((response) => response.json())
-//         .then((data) => {
-//           this.users = data.data;
-//         })
-//         .catch((error) => console.error("can not found data", error));
-//     },
-
-//     post() {
-//       fetch("https://67ce9cbb125cd5af757b3855.mockapi.io/data ", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(this.users),
-//       })
-//         .then((response) => response.json())
-//         .catch((error) => console.error(error));
-//     },
-//   };
-// }
-
-// get data from Api
-let btn2 = document.querySelector(".btn2");
-
-var data = [];
-btn2.onclick = () => {
-  $.ajax({
-    url: "https://reqres.in/api/users?page=1",
-    type: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    success: (response) => (
-      (data = response.data), console.log("البيانات التي تم الحصول عليها", data)
-    ),
-    error: (error) => console.log(error),
+  let trHead = document.createElement("tr");
+  headers.forEach((item) => {
+    let th = document.createElement("th");
+    th.textContent = item;
+    trHead.appendChild(th);
   });
-};
+  thead.appendChild(trHead);
 
-//  send data to Api
+  let tbody = document.querySelector("table tbody");
+  tbody.innerHTML = "";
 
-let btn3 = document.querySelector(".btn3");
+  data.forEach((item) => {
+    let tr = document.createElement("tr"),
+      tdID = document.createElement("td"),
+      tdf_name = document.createElement("td"),
+      tdl_name = document.createElement("td"),
+      td_email = document.createElement("td"),
+      td_del = document.createElement("td"),
+      btn = document.createElement("button");
 
-let newUser = {
-  id: 1,
-  first_name: "mohammed",
-  last_name: "hussein",
-  email: "usermohammed@gmail.com",
-};
+    tdID.textContent = item.id;
+    tdf_name.textContent = item.first_name;
+    tdl_name.textContent = item.last_name;
+    td_email.textContent = item.email;
+    btn.textContent = "Delete";
+    btn.dataset.id = item.id;
+    btn.addEventListener("click", (e) => {
+      deleteItem(e.target.dataset.id);
+    });
 
-btn3.onclick = () => {
-  $.ajax({
-    url: "https://67ce9cbb125cd5af757b3855.mockapi.io/data",
-    type: "POST",
-    data: JSON.stringify(newUser),
-    headers: {
-      "Content-Type": "application/json",
-    },
-    success: (response) => console.log("البيانات التي تم ارسالها ", response),
-    error: (error) => console.error("data not sent", error),
+    td_del.appendChild(btn);
+    tr.appendChild(tdID);
+    tr.appendChild(tdf_name);
+    tr.appendChild(tdl_name);
+    tr.appendChild(td_email);
+    tr.appendChild(td_del);
+    tbody.appendChild(tr);
   });
-};
+}
 
-// delete data from Api
+$.ajax({
+  url: "https://reqres.in/api/users?page=1",
+  type: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  success: (res) => {
+    localStorage.setItem("data", JSON.stringify(res.data));
+  },
+  error: (error) => console.error("Cannot get data", error),
+});
 
-let btn = document.querySelector(".btn");
-btn.onclick = () => {
-  $.ajax({
-    url: "https://67ce9cbb125cd5af757b3855.mockapi.io/data/1",
-    type: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    success: (response) =>
-      console.log(response, console.log("البيانات التي تم حذفها", response)),
-    error: (error) => console.error("data not sent", error),
-  });
-};
+function renderTable() {
+  let local_data = JSON.parse(localStorage.getItem("data")) || [];
+  table(local_data);
+}
+
+function deleteItem(id) {
+  let data = JSON.parse(localStorage.getItem("data")) || [];
+  let new_data = data.filter((item) => item.id != id);
+  localStorage.setItem("data", JSON.stringify(new_data));
+  renderTable();
+}
+
+renderTable();
